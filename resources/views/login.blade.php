1914530ce0481ee63d411f042daf075bec9164b5
AlfonsoCoronaR <acr.redb@gmail.com>
2021-10-12 17:25:31 -0500

    Modificando login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,29 +2,33 @@
 @section('contenido')
 
 <div class="container">
-    <div class="encabezado text-center">
-        <h1>Timeclock GESDES</h1>
-    </div>
     <div class="vh-100 row justify-content-center align-items-center">
-        <div class="card col col-lg-4 ">
-            <div class="card-header text-center">
-                <h5>Inicio de sesión</h5>
+        <div class="plantilla">
+            <div class="encabezado text-center mb-4">
+                <h1>Timeclock GESDES</h1>
             </div>
-            <div class="card-body">
-                <form action="">
-                    <div class="mb-3">
-                        <label for="usuario" class="form-label"> Usuario: </label>
-                        <input type="text" name="usuario" placeholder="usuario..." class="form-control">
+            <div class="row justify-content-center">
+                <div class="card col col-lg-4">
+                    <div class="card-header text-center">
+                        <h5>Inicio de sesión</h5>
                     </div>
-                    <div class="mb-3">
-                        <label for="password" class="form-label"> Contraseña: </label>
-                        <input type="password" name="password" placeholder="contraseña..." class="form-control">
+                    <div class="card-body">
+                        <form action="">
+                            <div class="mb-3">
+                                <label for="usuario" class="form-label"> Usuario: </label>
+                                <input type="text" name="usuario" placeholder="usuario..." class="form-control">
+                            </div>
+                            <div class="mb-3">
+                                <label for="password" class="form-label"> Contraseña: </label>
+                                <input type="password" name="password" placeholder="contraseña..." class="form-control">
+                            </div>
+            
+                            <input type="submit" value="Entrar" class="btn btn-outline-warning">
+                        </form>
                     </div>
-    
-                    <input type="submit" value="Entrar" class="btn btn-outline-warning">
-                </form>
-            </div>
-          </div> 
+                </div>
+            </div> 
+        </div>
     </div>
 </div>
 
